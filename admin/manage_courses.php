be8f1639ad5bb263b45265addf67c2ef64ded8e2
johnpauljuma <johnpauljuma29@gmail.com>
2023-09-21 22:04:09 +0300

    modified manage courses file

--- admin/manage_courses.php
+++ admin/manage_courses.php
@@ -6,9 +6,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Admin | manage courses</title>
     <style>
-        .container{
+        .container {
             height: fit-content;
             width: fit-content;
             margin: auto;
@@ -21,55 +22,104 @@
             box-shadow: 0 0 5px 0;
             border-radius: 10px;
             border-style: outset;
-          
         }
         
-        table{
+        table {
             margin: auto;
             margin-top: 2em;
             padding: 10px;
             box-shadow: 0 0 3px 0;
         }
-        th{
+
+        th {
             padding: 5px;
             text-align: left;
             box-shadow: 0 0 5px 0;
-
         }
-        td{
+
+        td {
             border: 1px;
             margin: 5px;
             text-align: left;
             padding: 10px;
         }
-        span{
+
+        span {
             color: red;
             margin-left: 0;
             margin-right: 2em;
         }
-        h3{
+
+        h3 {
             margin: 0;
             margin-top: 10px;
             margin-bottom: -10px;
-
         }
-        input[type="search"] {
-            padding: 8px;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-            margin-bottom: 10px;
-            width: 100%;
-            max-width: 300px; 
-            box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.1);
-            font-size: 16px;
-            background-color: #fff;
-            background-image: url('search-icon.png'); 
-            background-position: 10px 10px;
-            background-repeat: no-repeat;
-            text-indent: 30px;
-            float: right;
+
+        .h3 {
+            display: inline-flex;
+            width: 50%;
+        }
+
+        .cover {
             display: flex;
+            margin-bottom: -20px;
+            margin-top: 1em;
+        }
+
+        * {
+            box-sizing: border-box;
+        }
+
+        .form_container {
+            display: inline-flex;
             right: 0;
+            float: right;
+            justify-content: flex-end;
+            width: 100%;
+            padding: 0;
+        }
+
+        form {
+            float: right;
+            right: 0;
+            height: 40px;
+            display: flex;
+            max-width: 300px;
+        }
+
+        form.example input[type=text] {
+            padding: 10px;
+            font-size: 17px;
+            border: 1px solid grey;
+            float: left;
+            width: 80%;
+            background: #f1f1f1;
+            border-radius: 20px 0 0 20px;
+            
+        }
+
+        form.example button {
+            float: left;
+            width: 20%;
+            padding: 10px;
+            background: #2196F3;
+            color: white;
+            font-size: 17px;
+            border: 1px solid grey;
+            border-left: none;
+            border-radius: 0 20px 20px 0;
+            cursor: pointer;
+        }
+
+        form.example button:hover {
+            background: #0b7dda;
+        }
+
+        form.example::after {
+            content: "";
+            clear: both;
+            display: table;
         }
     </style>
 </head>
@@ -77,8 +127,15 @@
     
         <center><h2>Manage courses</h2></center>
     <div class="container">
-        <center><h3>Course Details</h3></center>
-        <input type="search" name="search" placeholder="Enter corese code to search">
+        <div class="cover">
+            <div class="h3"><h3>Course Details</h3></div>
+            <div class="form_container">
+                <form class="example" action="/action_page.php">
+                    <input type="text" placeholder="Search course..." name="search2">
+                    <button type="submit"><i class="fa fa-search"></i></button>
+                </form>
+            </div>
+        </div>
         <table>
             <tr>
                 <th>Course Code</th>
